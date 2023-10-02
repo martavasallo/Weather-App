684b0de2e6bce5bbf2d65eccf69a5bb7e68a2de0
marta vasallo <mvasalloperez@gmail.com>
2023-10-02 16:35:22 +0100

    Added main features

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const APIKEY = '376628fc58a96623637f47269d74581f';
 
 const input = document.getElementById('input');
-const city = document.getElementById('city');
 const date = document.getElementById('date');
+const city = document.getElementById('city');
+const country = document.getElementById('country');
 const temperature = document.getElementById('temperature');
+const main = document.getElementById('main');
 const description = document.getElementById('description');
 const icon = document.getElementById('icon');
 const currentLocation = document.getElementById('currentLocation');
@@ -30,18 +32,38 @@ const fectchWeatherByCoordinates = (latitude, longitude) => {
 };
 
 const showWeather = (data) => {
-  // console.log(data);
+  console.log(data.sys.country);
+  // CITY NAME
   city.innerText = data.name;
+
+  // COUNTRY
+  country.innerText = data.sys.country
+
+  // TEMPERATURE
   temperature.innerText = `${Math.round(data.main.temp)}°C`;
-  description.innerText = data.weather[0].description;
+
+  // MAIN
+  main.innerText = data.weather[0].main;
+
+  // DESCCRIPTION
+  const descriptionText = data.weather[0].description;
+  const descriptionCapitalized = descriptionText.charAt(0).toUpperCase() + descriptionText.slice(1);
+  description.innerText = descriptionCapitalized;
+
+  // ICON
   icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
+  // TIME
   const today = new Date();
   const localOffset = data.timezone + today.getTimezoneOffset() * 60;
   const localDate = new Date(today.setUTCSeconds(localOffset));
-  const options = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-  const formattedDate = localDate.toLocaleDateString("en-US", options);
-  date.innerText = formattedDate;
+
+  const hour = localDate.getHours().toString().padStart(2, '0');
+  const minute = localDate.getMinutes().toString().padStart(2, '0');
+
+  const formattedTime = `Local time ${hour}:${minute}`;
+  date.innerText = formattedTime;
+
 }
 
 const fetchCurrentPosition = (event) => {
@@ -50,8 +72,6 @@ const fetchCurrentPosition = (event) => {
     fectchWeatherByCoordinates(data.coords.latitude, data.coords.longitude)
   })
 }
-
-
 
 const form = document.querySelector('form');
 searchForm.addEventListener('submit', fetchWeatherByCity);
