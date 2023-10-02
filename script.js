1c2ee363aab303e2445b65b0dee6e47c3b15b815
marta vasallo <mvasalloperez@gmail.com>
2023-10-02 12:17:02 +0100

    fixed fetching error. Displaying all fields now

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const APIKEY = '376628fc58a96623637f47269d74581f';
 
 const input = document.getElementById('input');
 const city = document.getElementById('city');
+const date = document.getElementById('date');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+
 
 const fetchWeatherByCity = (event) => {
   event.preventDefault();
@@ -9,19 +13,35 @@ const fetchWeatherByCity = (event) => {
   const urlCityName = `http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&appid=${APIKEY}`
   fetch(urlCityName)
   .then(response => response.json())
-  .then((data) => {
-  console.log(data);
-  });
-}
+  .then(data => {
+    const latitude = data[0].lat
+    const longitude = data[0].lon
+    fectchWeatherByCoordinates(latitude, longitude);
+  })
+};
 
-const fectchWeatherByCoordinates = (coordinates) => {
-  const urlCoordinates = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${APIKEY}`
+const fectchWeatherByCoordinates = (latitude, longitude) => {
+  const urlCoordinates = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`
   fetch(urlCoordinates)
   .then(response => response.json())
-  .then((data) => {
+  .then(showWeather)
+};
+
+const showWeather = (data) => {
   console.log(data);
-  });
+  city.innerText = data.name;
+  temperature.innerText = `${Math.round(data.main.temp - 273.15)}°C`;
+  description.innerText = data.weather[0].description;
+  icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+  const today = new Date();
+  const localOffset = data.timezone + today.getTimezoneOffset() * 60;
+  const localDate = new Date(today.setUTCSeconds(localOffset));
+  const options = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  const formattedDate = localDate.toLocaleDateString("en-US", options);
+  date.innerText = formattedDate;
 }
 
-const searchForm = document.getElementById('searchForm');
+
+const form = document.querySelector('form');
 searchForm.addEventListener('submit', fetchWeatherByCity);
